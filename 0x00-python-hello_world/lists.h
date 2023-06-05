e825d62b1b0729d702f5b0a6558120d8233275d5
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-05 05:00:54 -0700

    My initial commit

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,28 @@
+
+
+
+#ifndef lists_h
+#define lists_h
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - This is singly lists structure
+ * @n: integer
+ * @next: This will point to the next node
+ *
+ * Description: singly linked list
+ */
+
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
+int check_cycle(listint_t *list);
+void free_listint(listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
+size_t print_listint(const listint_t *h);
+
+#endif 
