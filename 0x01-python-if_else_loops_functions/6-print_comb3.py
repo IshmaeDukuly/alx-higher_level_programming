3d91a5495d15587c895ad7c163f2a3ba8ba63592
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-06 09:02:34 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,6 @@
 for n in range(0, 89):
     for n1 in range((n+1), 10):
         if (n != 8) or (n1 != 9):
+            print("{}{}, ".format(n, n1), end="")
+        else:
             print("{}{}".format(n, n1))
