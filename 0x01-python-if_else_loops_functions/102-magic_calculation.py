1340ad08e670048358cc2cd5705e15109578e9fa
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-06 17:56:01 -0700

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Author by: Ishmael"""
 def magic_calculation(n1, n2, n3):
     if n1 < n2:
         return (n3)
     if n3 > n2:
         return (n1 + n2)
-    return (n*n2 - n3)
+    return (n1*n2 - n3)
