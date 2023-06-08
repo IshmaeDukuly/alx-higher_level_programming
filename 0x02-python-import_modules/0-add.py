8ed7e3f13c4b6777c5ca8d9970c4bb15f9480030
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-08 04:19:52 -0700

    my initial commits

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ if _name_ == "_main_":
     from  add_0 import add
     n1 = 1
     n2 = 2
-    print("{} + {} = {}.format(n1, n2, add(n1, n2)))
+    print("{} + {} = {}".format(n1, n2, add(n1, n2)))
 
