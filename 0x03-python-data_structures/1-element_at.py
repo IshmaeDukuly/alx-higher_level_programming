ee9210ba36fba7cc59a4dd32837a29d6a91dcecb
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-11 08:24:54 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Author by: Ishmael Dukuly
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list) - 1:
         return null
     else:
         return my_list[idx]
