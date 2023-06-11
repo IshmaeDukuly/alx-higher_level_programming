27db2d5cf61fcfa2c3a42f22dd13c2f0afa32b0a
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-11 12:38:03 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
-         print()
+    for n in matrix:
+        print(" ".join("{:d}".format(m) for m in n))
