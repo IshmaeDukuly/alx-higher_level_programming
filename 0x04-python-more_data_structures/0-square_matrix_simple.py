878b55420a3369f6c96f818e5cc4b08ac1e32811
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-14 05:17:03 -0700

    initial commits

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    return list(map(lamba submat: list(map(lamba x: x**2, matrix))
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Auhtor by: Ishmael Dukuly
+def search_replace(my_list, search, replace):
+    return (list(map(lamba y: replace if y is search else y, my_list))
