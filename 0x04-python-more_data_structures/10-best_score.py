536324dc41700104347ea1c027decda3133cc56e
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-14 06:43:59 -0700

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def bes_score(my_dict):
     if my_dict is None or my_dict == {}:
         return None
-    highest = max(my_dict.values())
-    for key, val in my_dict.items():
-        if val is highest:
+    highest = max(my_dict.value())
+    for key, value in my_dict.items():
+        if value is highest:
             return key
