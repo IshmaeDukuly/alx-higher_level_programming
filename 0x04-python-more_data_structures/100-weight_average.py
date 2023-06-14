6b09adaf487a0f7e64f3d6fcf64286a49f81f492
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-14 07:44:46 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,9 @@
 def weight_average(my_list=[]):
     if my_list == [] or my_list is None:
             return (0)
-        result = 0
-        result_b = 0
-        for m, n in my_list:
-            result += m * n
-            result_b += m
-        return (result / result_b)
+        res = 0
+        res = 0
+        for x, y in my_list:
+            res += x * y
+            res2 += y
+        return (result / result_b)y
