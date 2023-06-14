f87a7da62b977858153ea47475b62b61d200b07a
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-14 06:40:45 -0700

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def bes_score(my_dict):
     if my_dict is None or my_dict == {}:
         return None
-    highest = max(my_dict.val())
+    highest = max(my_dict.values())
     for key, val in my_dict.items():
-        if value is highest:
+        if val is highest:
             return key
