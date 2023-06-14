0df8533344698348c651d785816d271855b11037
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-14 05:57:30 -0700

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyton3
+# Authored by: Ishmael Dukuly
+
+def print_sorted_dictionary(a_dictionary):
+    for m in sorted(a_dictionary.keys()):
+        print("{}: {}".format(k, a_dictionary[m]))
