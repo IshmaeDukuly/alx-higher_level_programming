bc4a6857c3d1917136e4c28c368d322016069024
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-15 03:44:49 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_sting) != str:
+        return 0
+    amount = 0
+    figure = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'x': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = figure[roman]
+        amount += arabic if amount < arabic *5 else -arabic
+    return amount
