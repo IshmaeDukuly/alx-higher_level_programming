a48eb1d36e65e87c02484834924ff333b1824dbf
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-27 15:33:12 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines the square"""
+
+class Square:
+    """A square representations"""
+
+    def __init__(self, size):
+        """Initializing the clas square
+        args: size - represents size of sqaure"""
+
+
+        self.__size = size
