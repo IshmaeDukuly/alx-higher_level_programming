ffa6e74e47457a3e1e72cd717e71f4fa5f6a0f89
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-28 15:12:07 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    amout = 0
+    for m in range(y):
+        try:
+            print(f"{my_list[m]", en="")
+            amount += 1
+        except Invalid Index:
+            break
+        print()
+        return(amount)
