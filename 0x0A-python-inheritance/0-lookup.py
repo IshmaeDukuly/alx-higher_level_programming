c2836fb58da65bce433d8b347f05de775d5b5380
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-10 07:41:58 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+"""
+Entai;s method of lookup that returns a lis of object
+"""
+
+def lookup(obj):
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""""
+This contains the class that inherite from the list
+"""
+
+class MyList():
+    """ Inherits
+
+    the methods:
+    print_sorted(self)
+    """
+
+    def print_sorted(self):
+        print(sorted(self)
