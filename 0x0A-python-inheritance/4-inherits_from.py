7293426d34881eb72959010f1995b73c0098e885
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-10 08:19:42 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-contains method inherites_from
+contains the method inherits_from
 output True if the object is the insatnce class that inherites 
 """
 
@@ -9,5 +9,4 @@ def inherites_from(obj, a_class):
     """
     Return: True if object is instance of class that inherites from
     """
-
     return (type(obj) is not a_class and issubclass(type(obj), a_class))
