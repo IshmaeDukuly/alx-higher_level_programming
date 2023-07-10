0f6a1ad206263011e80fc2eab7cc4fddb3429b40
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-10 16:34:29 -0700

    100-main.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,8 +2,8 @@
 """This is  module a class MyInt that has inherits from int"""
 
 
-    class MyInt(int):
-        """
+class MyInt(int):
+     """
          Methods:
         __init__(self, number)
         __eq__(self, others)
@@ -13,16 +13,15 @@
     def __init__(self, number):
         """now initialize number"""
         self.number
-    
-        def __eq__(self, others):
-            """
 
+     def __eq__(self, others):
+            """
             return: True if the both are not equal
             """
 
             return self.number != others
 
-        def __ne__(self, others):
+     def __ne__(self, others):
             """
             Return:
             True if both not equal
