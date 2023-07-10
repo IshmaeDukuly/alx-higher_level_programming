5fe99d7342304efe3c4c372d7409e3a70c7b6175
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-10 08:34:01 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/sur/bin/python3
+"""
+An empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """"The empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#Author: Ishmael Dukuly
+"""
+The empty class BaseGeometry
+with instance method area
+"""
+
+
+class BaseGeometry
+    """
+    The Methods:
+    ara(self)
+    
+    """
+
+    def area(self):
+        """Not implemented"""
+        raise Exception(""area() is not implemented")
