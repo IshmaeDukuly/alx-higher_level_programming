5d6f9a3791346664d1e2500c590a2fc7fe180230
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-11 07:18:51 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,5 +6,5 @@ Consists function that appends to  text file and returns num chars added
 
 def append_write(filename="", text=""):
     """appends to text file and returns num chars added"""
-    width open(filename, mode="a, encoding="utf-8") as f:
-    return (f.write(text))
+    width open(filename, mode="a", encoding="utf-8") as f:
+        return(f.write(text))
