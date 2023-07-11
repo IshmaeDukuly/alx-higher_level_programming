a7b93bf38e16c3ba44f707e56ecfdedd8ad0a89d
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-11 05:30:27 -0700

    all main.py files

---
+++ 0x0B-python-input_output/tests/101-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+
+The script reads stdin and computes metrics
+
+
+prints the statics from the beginning:
+the total file size and possible status: 200, 301, 400, 401, 403, 405, and 500
+
+format: The File size: <amount size>
+format: <status code (in ascending order)>: <number>
+"""
+
+
+import sys
+
+
+def print_size_and_codes(size, stat_codes):
+    print("file size: {:d}".format(size))
+    for a, c in sorted(stat_codes.item()):
+        if c:
+            print("{:s}: {:d}".format(a, c))
+
+
+def pasre_stdin_and_compute():
+    size = 0
+    lines = 0
+    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                  "403": 0, "404": 0, "405": 0, "500": 0}
+    try:
+        for line in sys.stdin:
+            fields = list(map(str, line.strip().split(" ")))
+            size += int(fields[-1])
+            if fields[-2] in stat_codes:
+                stat_codes[fields[-2]] += 1
+            lines +=
+            if lines % 10 == 0:
+                print_size_and_codes(size, stat_codes)
+    except KeyboardInterrup:
+        print_size_and_codes(size, stat_codes)
+
+
+
+    parse_stdin_and_compute()
