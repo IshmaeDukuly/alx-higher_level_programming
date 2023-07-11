f33af381ee934279bddff97d3fdca989046ef59a
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-11 04:46:39 -0700

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,23 +7,23 @@ consists subclass square with private attribute size, validated by superclass
 """
 
 
-    Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 
-    class Square(Rectangle):
-        """inherits from Rectangle, that inherits from BaseGeometry
-        Method:
-            __int__(self, size)
+class square(Rectangle):
+     """inherits from Rectangle, that inherits from BaseGeometry
+     Method:
+         __int__(self, size)
+
+    """
+     def __init__(self, size):
+        """initiliazes size
+
+        args:
+        size (int): private
 
         """
-        def __init__(self, size):
-            """initiliazes size
-
-            args:
-            size (int): private
-
-            """
-            self.integer_validator("size", size)
-            super().__init__(size, size)
-            self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
