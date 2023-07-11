827471740316dd28ed7892dfe2308fe66959a06a
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-11 06:44:26 -0700

    multiple files

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains function that writes to text file and returns num and chars written
+"""
+
+
+def write_flie(filename="", text=""):
+    """Writes text to file and num chars written"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return(f.write(text))
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contains function taht returns JSON representation of obj (string)
+"""
+
+def to_json_string(my_obj):
+    """Return to JSON representation of obj (string)
+    Args:
+        my_obj: python obj
+    Return: 
+        json string representation
+    """
+    import json
+
+    return json.dumps(my_obj)
