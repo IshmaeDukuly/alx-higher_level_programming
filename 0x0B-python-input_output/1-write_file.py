60aa2434b9090ed7545446cb4033cf6bc696195b
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-11 06:50:39 -0700

    3-to_json_string.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,7 @@ Contains function that writes to text file and returns num and chars written
 """
 
 
-def write_flie(filename="", text=""):
+def write_file(filename="", text=""):
     """Writes text to file and num chars written"""
     with open(filename, mode="w", encoding="utf-8") as f:
         return(f.write(text))
